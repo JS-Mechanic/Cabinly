283fd2986782af6d5a09dc66f220f59e0c107e72
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-09-03 12:44:36 +0330

    Connecting createCabin function to the form using useMutation hook

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -47,10 +47,21 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
-	const {register, handleSubmit} = useForm();
+	const {register, handleSubmit, reset} = useForm();
+	const queryClient = useQueryClient();
+
+	const {mutate, isLoading: isCreating} = useMutation({
+		mutationFn: createCabin,
+		onSuccess: () => {
+			toast.success("New cabin successfully created");
+			queryClient.invalidateQueries({queryKey: ["cabins"]});
+			reset();
+		},
+		onError: err => toast(err.message),
+	});
 
 	function onSubmit(data) {
-		console.log(data);
+		mutate(data);
 	}
 
 	return (
@@ -90,7 +101,7 @@ function CreateCabinForm() {
 				<Button variation="secondary" type="reset">
 					Cancel
 				</Button>
-				<Button>Add cabin</Button>
+				<Button disabled={isCreating}>Add cabin</Button>
 			</FormRow>
 		</Form>
 	);
