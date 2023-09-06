4b3c1dca194338b33ea21a9c24f4bc3b8a264a5c
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-09-06 10:54:55 +0330

    Importing relevant modules

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -3,6 +3,8 @@ import {formatCurrency} from "../../utils/helpers.js";
 import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {deleteCabin} from "../../services/apiCabins.js";
 import toast from "react-hot-toast";
+import {useState} from "react";
+import CreateCabinForm from "./CreateCabinForm.jsx";
 
 const TableRow = styled.div`
 	display: grid;
