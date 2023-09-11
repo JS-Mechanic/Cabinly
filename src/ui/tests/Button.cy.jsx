a1d38a7c10121bf01a72643793cc6c263bbf5f72
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-09-11 23:35:45 +0330

    Testing other sizes and variations of Button component

--- src/ui/tests/Button.cy.jsx
+++ src/ui/tests/Button.cy.jsx
@@ -7,6 +7,7 @@ describe("Button component", () => {
 				Button
 			</Button>,
 		);
+
 		cy.get("button")
 			.should("have.text", "Button")
 			.should("have.css", "font-size", "19.2px")
@@ -14,5 +15,33 @@ describe("Button component", () => {
 			.should("have.css", "text-transform", "uppercase")
 			.should("have.css", "font-weight", "600")
 			.should("have.css", "text-align", "center");
+	});
+
+	it("mounts a medium secondary Button component ", () => {
+		cy.mount(
+			<Button size="medium" variation="secondary">
+				Button
+			</Button>,
+		);
+
+		cy.get("button")
+			.should("have.text", "Button")
+			.should("have.css", "font-size", "22.4px")
+			.should("have.css", "padding", "19.2px 25.6px")
+			.should("have.css", "font-weight", "500");
+	});
+
+	it("mounts a large danger Button component ", () => {
+		cy.mount(
+			<Button size="large" variation="danger">
+				Button
+			</Button>,
+		);
+
+		cy.get("button")
+			.should("have.text", "Button")
+			.should("have.css", "font-size", "25.6px")
+			.should("have.css", "padding", "19.2px 38.4px")
+			.should("have.css", "font-weight", "500");
 	});
 });
