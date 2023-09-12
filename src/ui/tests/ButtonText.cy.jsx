f667211a47f8ebcd7577dc52f744732f51b5934c
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-09-12 17:09:35 +0330

    Testing ButtonText component with browser specification

--- src/ui/tests/ButtonText.cy.jsx
+++ src/ui/tests/ButtonText.cy.jsx
@@ -1,7 +1,7 @@
 import ButtonText from "../ButtonText.jsx";
 
 describe("ButtonText component", () => {
-	it("should mounts ButtonText component", () => {
+	it("should mounts ButtonText component on chrome", {browser: "chrome"}, () => {
 		cy.mount(<ButtonText>Test</ButtonText>);
 		cy.get("button").should("have.css", "font-weight", "500");
 		cy.get("button").should("have.css", "text-align", "center");
@@ -12,5 +12,27 @@ describe("ButtonText component", () => {
 			"rgba(0, 0, 0, 0) none repeat scroll 0% 0% / auto padding-box border-box",
 		);
 		cy.get("button").should("have.css", "border", "0px none rgb(0, 0, 0)");
+	});
+
+	it("should mounts ButtonText component on electron", {browser: "electron"}, () => {
+		cy.mount(<ButtonText>Test</ButtonText>);
+		cy.get("button").should("have.css", "font-weight", "500");
+		cy.get("button").should("have.css", "text-align", "center");
+		cy.get("button").should("have.css", "transition", "all 0.3s ease 0s");
+		cy.get("button").should(
+			"have.css",
+			"background",
+			"rgba(0, 0, 0, 0) none repeat scroll 0% 0% / auto padding-box border-box",
+		);
+		cy.get("button").should("have.css", "border", "0px none rgb(0, 0, 0)");
+	});
+
+	it("should mounts ButtonText component on firefox", {browser: "firefox"}, () => {
+		cy.mount(<ButtonText>Test</ButtonText>);
+		cy.get("button").should("have.css", "font-weight", "500");
+		cy.get("button").should("have.css", "text-align", "center");
+		cy.get("button").should("have.css", "transition", "all 0.3s ease 0s");
+		cy.get("button").should("have.css", "background", "none");
+		cy.get("button").should("have.css", "border", "0px rgb(46, 52, 54)");
 	});
 });
