5739db9b57cd8804184600abfad1225e891d9ec7
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-09-15 19:15:20 +0330

    Reset settings table to default values

--- cypress/e2e/settings/Settings.cy.js
+++ cypress/e2e/settings/Settings.cy.js
@@ -22,4 +22,19 @@ describe("Settings section", async () => {
 		cy.get("#max-guests").should("have.value", 5);
 		cy.get("#breakfast-price").should("have.value", 20);
 	});
+
+	it("should reset settings", () => {
+		cy.visit("/");
+		cy.contains("span", "Settings").click();
+		cy.get("#min-nights").clear().type(3).blur();
+		cy.get("#max-nights").clear().type(90).blur();
+		cy.get("#max-guests").clear().type(10).blur();
+		cy.get("#breakfast-price").clear().type(15).blur();
+
+		cy.wait(3000);
+		cy.get("#min-nights").should("have.value", 3);
+		cy.get("#max-nights").should("have.value", 90);
+		cy.get("#max-guests").should("have.value", 10);
+		cy.get("#breakfast-price").should("have.value", 15);
+	});
 });
