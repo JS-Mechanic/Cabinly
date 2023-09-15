f59b02228fcdc0d701a50bd31c1bca0f1128f9f6
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-09-15 19:14:40 +0330

    Checking blur feature to update settings

--- cypress/e2e/settings/Settings.cy.js
+++ cypress/e2e/settings/Settings.cy.js
@@ -7,4 +7,19 @@ describe("Settings section", async () => {
 		cy.contains("h1", "Update hotel settings");
 		cy.get("input").should("have.length", 4);
 	});
+
+	it("checks blur feature to update settings", () => {
+		cy.visit("/");
+		cy.contains("span", "Settings").click();
+		cy.get("#min-nights").clear().type(2).blur();
+		cy.get("#max-nights").clear().type(50).blur();
+		cy.get("#max-guests").clear().type(5).blur();
+		cy.get("#breakfast-price").clear().type(20).blur();
+
+		cy.wait(3000);
+		cy.get("#min-nights").should("have.value", 2);
+		cy.get("#max-nights").should("have.value", 50);
+		cy.get("#max-guests").should("have.value", 5);
+		cy.get("#breakfast-price").should("have.value", 20);
+	});
 });
