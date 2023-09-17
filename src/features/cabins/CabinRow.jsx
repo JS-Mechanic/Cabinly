819abcfbcb5a2b1e9ca4bfe57289d0671406bc8e
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-09-17 13:27:21 +0330

    Implementing reusable table component using compound-component and render-prop patterns

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -7,18 +7,7 @@ import {HiPencil, HiTrash} from "react-icons/hi";
 import {useCreateCabin} from "./useCreateCabin.js";
 import Modal from "../../ui/Modal.jsx";
 import ConfirmDelete from "../../ui/ConfirmDelete.jsx";
-
-const TableRow = styled.div`
-	display: grid;
-	grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-	column-gap: 2.4rem;
-	align-items: center;
-	padding: 1.4rem 2.4rem;
-
-	&:not(:last-child) {
-		border-bottom: 1px solid var(--color-grey-100);
-	}
-`;
+import Table from "../../ui/Table.jsx";
 
 const Img = styled.img`
 	display: block;
@@ -60,7 +49,7 @@ export default function CabinRow({cabin}) {
 	}
 
 	return (
-		<TableRow role="row" className="table-row">
+		<Table.Row>
 			<Img src={image} />
 			<Cabin>{name}</Cabin>
 			<div>Fits up to {maxCapacity} guests</div>
@@ -95,6 +84,6 @@ export default function CabinRow({cabin}) {
 					</Modal.Window>
 				</Modal>
 			</div>
-		</TableRow>
+		</Table.Row>
 	);
 }
