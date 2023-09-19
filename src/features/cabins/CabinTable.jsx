7c7f1f2f519e9dc456baead022d14890836e155c
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-09-19 12:04:11 +0330

    Implementing filter component and using it to filter cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,10 +3,21 @@ import CabinRow from "./CabinRow.jsx";
 import {useCabins} from "./useCabins.js";
 import Table from "../../ui/Table.jsx";
 import Menus from "../../ui/Menus.jsx";
+import {useSearchParams} from "react-router-dom";
 
 export function CabinTable() {
 	const {isLoading, cabins} = useCabins();
+	const [searchParams] = useSearchParams();
 	if (isLoading) return <Spinner />;
+
+	const filterValue = searchParams.get("discount") || "all";
+
+	let filteredCabins;
+	if (filterValue === "all") filteredCabins = cabins;
+	else if (filterValue === "no-discount")
+		filteredCabins = cabins.filter(cabin => cabin.discount === 0);
+	else if (filterValue === "with-discount")
+		filteredCabins = cabins.filter(cabin => cabin.discount > 0);
 	return (
 		<Menus>
 			<Table columns="0.65fr 1.5fr 2.2fr 1.2fr 1.2fr 0.2fr">
@@ -18,7 +29,10 @@ export function CabinTable() {
 					<div>Discount</div>
 					<div></div>
 				</Table.Header>
-				<Table.Body data={cabins} render={cabin => <CabinRow cabin={cabin} key={cabin.id} />} />
+				<Table.Body
+					data={filteredCabins}
+					render={cabin => <CabinRow cabin={cabin} key={cabin.id} />}
+				/>
 			</Table>
 		</Menus>
 	);
