13ded59c0fdef1b1a071d3fc56786174f82531d5
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-09-24 21:30:07 +0330

    Adding, Editing and removing a cabin e2e tests

--- cypress/e2e/cabins/Cabins.cy.js
+++ cypress/e2e/cabins/Cabins.cy.js
@@ -5,7 +5,7 @@ describe("Cabins section", async () => {
 		cy.contains("h1", "Dashboard");
 		cy.contains("span", "Cabins").click();
 		cy.contains("h1", "All cabins");
-		cy.get(".row").should("have.length", 5);
+		cy.get(".row").should("have.length", 8);
 	});
 	it("should toggle create-cabin-form using Cancel button", () => {
 		cy.visit("/");
@@ -36,78 +36,76 @@ describe("Cabins section", async () => {
 		cy.get("#description").click().type("This is a luxury cabin created by cypress e2e test.");
 		cy.get("#image").selectFile("src/data/cabins/cabin-008.jpg");
 		cy.contains("button", "Create new cabin").click();
+		cy.wait(5000);
 	});
 
-	// it("should edit cabin", () => {
-	// 	cy.visit("/");
-	// 	cy.contains("span", "Cabins").click();
-	// 	cy.get(".row").contains("div", "New e2e cabin").siblings().children().children().click();
-	// 	cy.get("button").contains("span", "Edit").click({force: true});
-	// 	cy.get("#name").click().clear().type("Edited cabin");
-	// 	cy.get("#maxCapacity").click().clear().type(15);
-	// 	cy.get("#regularPrice").click().clear().type(1000);
-	// 	cy.get("#discount").click().clear().type(100);
-	// 	cy.get("#description").click().clear().type("This is a luxury edited.");
-	// 	cy.contains("button", "Edit cabin").click({force: true});
-	// });
-	// it("should check edited cabin", () => {
-	// 	cy.visit("/");
-	// 	cy.contains("span", "Cabins").click();
-	//
-	// 	cy.wait(5000)
-	// 		.get(".row")
-	// 		.contains("div", "Edited cabin")
-	// 		.siblings(".maxCapacity")
-	// 		.should("have.text", "Fits up to 15 guests");
-	//
-	// 	cy.get(".row")
-	// 		.contains("div", "Edited cabin")
-	// 		.siblings(".price")
-	// 		.should("have.text", "$1,000.00");
-	//
-	// 	cy.get(".row")
-	// 		.contains("div", "Edited cabin")
-	// 		.siblings(".discount")
-	// 		.should("have.text", "$100.00");
-	// });
-	// it("should reset edited cabin", () => {
-	// 	cy.visit("/");
-	// 	cy.contains("span", "Cabins").click();
-	// 	cy.wait(3000)
-	// 		.get(".row")
-	// 		.contains("div", "Edited cabin")
-	// 		.siblings()
-	// 		.children()
-	// 		.children()
-	// 		.click();
-	// 	cy.get("button").contains("span", "Edit").click({force: true});
-	// 	cy.get("#name").click().clear().type("New e2e cabin");
-	// 	cy.get("#maxCapacity").click().clear().type(10);
-	// 	cy.get("#regularPrice").click().clear().type(500);
-	// 	cy.get("#discount").click().clear().type(50);
-	// 	cy.get("#description")
-	// 		.click()
-	// 		.clear()
-	// 		.type("This is a luxury cabin created by cypress e2e test.");
-	// 	cy.contains("button", "Edit cabin").click();
-	// });
-
-	it("should delete added cabin", () => {
+	it("should edit cabin", () => {
 		cy.visit("/");
 		cy.contains("span", "Cabins").click();
-		cy.wait(5000)
+		cy.get(".row").contains("div", "New e2e cabin").siblings().children().children().click();
+		cy.get("button").contains("span", "Edit").click({force: true});
+		cy.get("#name").click().clear().type("Edited cabin");
+		cy.get("#maxCapacity").click().clear().type(15);
+		cy.get("#regularPrice").click().clear().type(1000);
+		cy.get("#discount").click().clear().type(100);
+		cy.get("#description").click().clear().type("This is a luxury edited.");
+		cy.contains("button", "Edit cabin").click();
+		cy.wait(5000);
+	});
+	it("should check edited cabin", () => {
+		cy.visit("/cabins");
+		// cy.contains("span", "Cabins").click();
+
+		cy.get(".row")
+			.contains("div", "Edited cabin")
+			.siblings(".maxCapacity")
+			.should("have.text", "Fits up to 15 guests");
+
+		cy.get(".row")
+			.contains("div", "Edited cabin")
+			.siblings(".price")
+			.should("have.text", "$1,000.00");
+
+		cy.get(".row")
+			.contains("div", "Edited cabin")
+			.siblings(".discount")
+			.should("have.text", "$100.00");
+	});
+	it("should reset edited cabin", () => {
+		cy.visit("/");
+		cy.contains("span", "Cabins").click();
+		cy.wait(3000)
 			.get(".row")
-			.contains("div", "New e2e cabin")
+			.contains("div", "Edited cabin")
 			.siblings()
 			.children()
 			.children()
 			.click();
+		cy.get("button").contains("span", "Edit").click({force: true});
+		cy.get("#name").click().clear().type("New e2e cabin");
+		cy.get("#maxCapacity").click().clear().type(10);
+		cy.get("#regularPrice").click().clear().type(500);
+		cy.get("#discount").click().clear().type(50);
+		cy.get("#description")
+			.click()
+			.clear()
+			.type("This is a luxury cabin created by cypress e2e test.");
+		cy.contains("button", "Edit cabin").click();
+		cy.wait(5000);
+	});
+
+	it("should delete added cabin", () => {
+		cy.visit("/");
+		cy.contains("span", "Cabins").click();
+		cy.get(".row").contains("div", "New e2e cabin").siblings().children().children().click();
 		cy.get("button").contains("span", "Delete").click({force: true});
-		cy.get("#root").siblings().children().contains("button", "Delete").click().wait(5000);
+		cy.get("#root").siblings().children().contains("button", "Delete").click();
+		cy.wait(5000);
 	});
 	it("checks that cabin is successfully deleted", () => {
 		cy.visit("/");
 		cy.contains("span", "Cabins").click();
-		cy.get(".row").should("have.length", 5);
+		cy.get(".row").should("have.length", 8);
+		cy.wait(5000);
 	});
 });
