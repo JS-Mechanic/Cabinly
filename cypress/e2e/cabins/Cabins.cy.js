f8813e8e4e45b66f50c509f28f29992b4057ea65
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-09-27 21:34:47 +0330

    Refactoring cabins e2e tests

--- cypress/e2e/cabins/Cabins.cy.js
+++ cypress/e2e/cabins/Cabins.cy.js
@@ -10,20 +10,20 @@ describe("Cabins section", async () => {
 	it("should toggle create-cabin-form using Cancel button", () => {
 		cy.visit("/");
 		cy.contains("span", "Cabins").click();
-		cy.get("#create-cabin-form").should("not.exist");
+		cy.get("[data-test-id='create-cabin-form']").should("not.exist");
 		cy.contains("button", "Add new cabin").click();
-		cy.get("#create-cabin-form").should("be.visible");
+		cy.get("[data-test-id='create-cabin-form']").should("be.visible");
 		cy.contains("button", "Cancel").click();
-		cy.get("#create-cabin-form").should("not.exist");
+		cy.get("[data-test-id='create-cabin-form']").should("not.exist");
 	});
 	it("should toggle create-cabin-form using X button", () => {
 		cy.visit("/");
 		cy.contains("span", "Cabins").click();
-		cy.get("#create-cabin-form").should("not.exist");
+		cy.get("[data-test-id='create-cabin-form']").should("not.exist");
 		cy.contains("button", "Add new cabin").click();
-		cy.get("#create-cabin-form").should("be.visible");
-		cy.get("#modal-x-close").click();
-		cy.get("#create-cabin-form").should("not.exist");
+		cy.get("[data-test-id='create-cabin-form']").should("be.visible");
+		cy.get("[data-test-id='modal-x-close']").click();
+		cy.get("[data-test-id='create-cabin-form']").should("not.exist");
 	});
 	it("should add new cabin", () => {
 		cy.visit("/");
@@ -35,52 +35,34 @@ describe("Cabins section", async () => {
 		cy.get("#discount").click().clear().type(50);
 		cy.get("#description").click().type("This is a luxury cabin created by cypress e2e test.");
 		cy.get("#image").selectFile("src/data/cabins/cabin-008.jpg");
-		cy.contains("button", "Create new cabin").click();
-		cy.wait(5000);
+		cy.contains("button", "Create new cabin")
+			.click()
+			.then(async () => {
+				cy.get(".row", {timeout: 10000}).should("have.length", 9);
+			});
 	});
 
 	it("should edit cabin", () => {
 		cy.visit("/");
 		cy.contains("span", "Cabins").click();
-		cy.get(".row").contains("div", "New e2e cabin").siblings().children().children().click();
+		cy.get("[data-test-id='toggle-button-New e2e cabin']").click();
 		cy.get("button").contains("span", "Edit").click({force: true});
 		cy.get("#name").click().clear().type("Edited cabin");
 		cy.get("#maxCapacity").click().clear().type(15);
 		cy.get("#regularPrice").click().clear().type(1000);
 		cy.get("#discount").click().clear().type(100);
 		cy.get("#description").click().clear().type("This is a luxury edited.");
-		cy.contains("button", "Edit cabin").click();
-		cy.wait(5000);
-	});
-	it("should check edited cabin", () => {
-		cy.visit("/cabins");
-		// cy.contains("span", "Cabins").click();
-
-		cy.get(".row")
-			.contains("div", "Edited cabin")
-			.siblings(".maxCapacity")
-			.should("have.text", "Fits up to 15 guests");
-
-		cy.get(".row")
-			.contains("div", "Edited cabin")
-			.siblings(".price")
-			.should("have.text", "$1,000.00");
-
-		cy.get(".row")
-			.contains("div", "Edited cabin")
-			.siblings(".discount")
-			.should("have.text", "$100.00");
+		cy.contains("button", "Edit cabin")
+			.click()
+			.then(async () => {
+				cy.get("[data-test-id='toggle-button-Edited cabin']", {timeout: 5000}).should("exist");
+				cy.get(".row").should("have.length", 9);
+			});
 	});
 	it("should reset edited cabin", () => {
 		cy.visit("/");
 		cy.contains("span", "Cabins").click();
-		cy.wait(3000)
-			.get(".row")
-			.contains("div", "Edited cabin")
-			.siblings()
-			.children()
-			.children()
-			.click();
+		cy.get("[data-test-id='toggle-button-Edited cabin']").click();
 		cy.get("button").contains("span", "Edit").click({force: true});
 		cy.get("#name").click().clear().type("New e2e cabin");
 		cy.get("#maxCapacity").click().clear().type(10);
@@ -90,22 +72,23 @@ describe("Cabins section", async () => {
 			.click()
 			.clear()
 			.type("This is a luxury cabin created by cypress e2e test.");
-		cy.contains("button", "Edit cabin").click();
-		cy.wait(5000);
+		cy.contains("button", "Edit cabin")
+			.click()
+			.then(async () => {
+				cy.get("[data-test-id='toggle-button-New e2e cabin']", {timeout: 5000}).should("exist");
+			});
 	});
 
 	it("should delete added cabin", () => {
 		cy.visit("/");
 		cy.contains("span", "Cabins").click();
-		cy.get(".row").contains("div", "New e2e cabin").siblings().children().children().click();
+		cy.get("[data-test-id='toggle-button-New e2e cabin']").click();
 		cy.get("button").contains("span", "Delete").click({force: true});
-		cy.get("#root").siblings().children().contains("button", "Delete").click();
-		cy.wait(5000);
-	});
-	it("checks that cabin is successfully deleted", () => {
-		cy.visit("/");
-		cy.contains("span", "Cabins").click();
-		cy.get(".row").should("have.length", 8);
-		cy.wait(5000);
+		cy.get("[data-test-id='confirm-delete']")
+			.click()
+			.then(async () => {
+				cy.get("[data-test-id='toggle-button-New e2e cabin']", {timeout: 5000}).should("not.exist");
+				cy.get(".row").should("have.length", 8);
+			});
 	});
 });
