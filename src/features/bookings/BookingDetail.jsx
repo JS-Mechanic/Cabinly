ba84db426f8d9b65f0af7cbf64d0fe4f38903eac
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-10-01 13:55:57 +0330

    Using useBooking custom hook + reformatting by prettier

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -8,45 +8,47 @@ import ButtonGroup from "../../ui/ButtonGroup";
 import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 
-import { useMoveBack } from "../../hooks/useMoveBack";
+import {useMoveBack} from "../../hooks/useMoveBack";
+import {useBooking} from "./useBooking.js";
+import Spinner from "../../ui/Spinner.jsx";
 
 const HeadingGroup = styled.div`
-  display: flex;
-  gap: 2.4rem;
-  align-items: center;
+	display: flex;
+	gap: 2.4rem;
+	align-items: center;
 `;
 
 function BookingDetail() {
-  const booking = {};
-  const status = "checked-in";
+	const {booking, isLoading} = useBooking();
+	const moveBack = useMoveBack();
 
-  const moveBack = useMoveBack();
+	if (isLoading) return <Spinner />;
 
-  const statusToTagName = {
-    unconfirmed: "blue",
-    "checked-in": "green",
-    "checked-out": "silver",
-  };
+	const statusToTagName = {
+		unconfirmed: "blue",
+		"checked-in": "green",
+		"checked-out": "silver",
+	};
 
-  return (
-    <>
-      <Row type="horizontal">
-        <HeadingGroup>
-          <Heading as="h1">Booking #X</Heading>
-          <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
-        </HeadingGroup>
-        <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
-      </Row>
+	return (
+		<>
+			<Row type="horizontal">
+				<HeadingGroup>
+					<Heading as="h1">Booking #{booking.id}</Heading>
+					<Tag type={statusToTagName[booking.status]}>{booking.status.replace("-", " ")}</Tag>
+				</HeadingGroup>
+				<ButtonText onClick={moveBack}>&larr; Back</ButtonText>
+			</Row>
 
-      <BookingDataBox booking={booking} />
+			<BookingDataBox booking={booking} />
 
-      <ButtonGroup>
-        <Button variation="secondary" onClick={moveBack}>
-          Back
-        </Button>
-      </ButtonGroup>
-    </>
-  );
+			<ButtonGroup>
+				<Button variation="secondary" onClick={moveBack}>
+					Back
+				</Button>
+			</ButtonGroup>
+		</>
+	);
 }
 
 export default BookingDetail;
