e8284b39151f09b61b010b8b6e3f6fad04e1b4a5
Milad Sadeghi DM <MiladSadeghiDM@Gmail.com>
2023-10-16 23:14:21 +0330

    Defining a test to check that bookings are successfully fetched

--- cypress/e2e/bookings/Bookings.cy.js
+++ cypress/e2e/bookings/Bookings.cy.js
@@ -1,1 +1,13 @@
-describe("Bookings section", async () => {});
+describe("Bookings section", async () => {
+	it("checks that bookings are successfully fetched", () => {
+		cy.visit("/");
+		cy.url().should("include", "/dashboard");
+		cy.contains("span", "Bookings")
+			.click()
+			.then(async () => {
+				cy.url().should("contain", "/bookings");
+				cy.contains("h1", "All bookings");
+				cy.get(".row", {timeout: 10000}).should("have.lengthOf.lte", 10);
+			});
+	});
+});
