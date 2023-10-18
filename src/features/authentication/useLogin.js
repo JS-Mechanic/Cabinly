b468203f6fe8c03e2c0fda3e9fe2456c80364ed3
Milad Sadeghi DM <MiladSadeghiDM@Gmail.com>
2023-10-18 14:46:05 +0330

    Saving authenticated user on react query cache to prevent re-fetch

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,13 +1,15 @@
-import {useMutation} from "@tanstack/react-query";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {login as loginApi} from "../../services/apiAuth.js";
 import {useNavigate} from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+	const queryClient = useQueryClient();
 	const navigate = useNavigate();
 	const {mutate: login, isLoading: isLoggingIn} = useMutation({
 		mutationFn: ({email, password}) => loginApi({email, password}),
-		onSuccess: () => {
+		onSuccess: user => {
+			queryClient.setQueriesData(["user"], user);
 			toast.success("You are successfully logged in");
 			navigate("/dashboard");
 		},
