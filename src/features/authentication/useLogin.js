9d82ba6435de128126b306560c4a9bb527986cd1
Milad Sadeghi DM <MiladSadeghiDM@Gmail.com>
2023-10-22 22:31:56 +0330

    Fixing missed setting in useLogin custom hook

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -11,7 +11,7 @@ export function useLogin() {
 		onSuccess: user => {
 			queryClient.setQueryData(["user"], user.user);
 			toast.success("You are successfully logged in");
-			navigate("/dashboard");
+			navigate("/dashboard", {replace: true});
 		},
 		onError: e => {
 			toast.error(e.message);
