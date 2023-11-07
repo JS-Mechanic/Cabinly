b725c89a1ed633efa6f0e9e0eb4c50ae361a075e
Milad Sadeghi <miladsadeghidm@gmail.com>
2023-11-07 20:07:00 +0330

    Updating storybook addons list

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,11 +2,13 @@
 const config = {
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 	addons: [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/addon-onboarding",
-		"@storybook/addon-interactions",
-	],
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-onboarding",
+        "@storybook/addon-interactions",
+        "@storybook/addon-themes",
+        "@storybook/themes"
+    ],
 	framework: {
 		name: "@storybook/react-vite",
 		options: {},
