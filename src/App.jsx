7c909eaead65c30bdda66ccaa43edc37f1e7d82d
Milad Sadeghi <miladsadeghidm@gmail.com>
2023-12-01 18:31:28 +0330

    Wrapping routes inside Suspense component

--- src/App.jsx
+++ src/App.jsx
@@ -33,26 +33,28 @@ export default function App() {
 			<ReactQueryDevtools initialIsOpen={false} />
 			<GlobalStyles />
 			<BrowserRouter>
-				<Routes>
-					<Route
-						element={
-							<ProtectedRoute>
-								<AppLayout />
-							</ProtectedRoute>
-						}>
-						<Route index element={<Navigate replace to="/dashboard" />} />
-						<Route path="dashboard" element={<Dashboard />} />
-						<Route path="bookings" element={<Bookings />} />
-						<Route path="bookings/:bookingId" element={<Booking />} />
-						<Route path="checkin/:bookingId" element={<CheckIn />} />
-						<Route path="cabins" element={<Cabins />} />
-						<Route path="users" element={<Users />} />
-						<Route path="settings" element={<Settings />} />
-						<Route path="account" element={<Account />} />
-					</Route>
-					<Route path="login" element={<Login />} />
-					<Route path="*" element={<PageNotFound />} />
-				</Routes>
+				<Suspense fallback={null}>
+					<Routes>
+						<Route
+							element={
+								<ProtectedRoute>
+									<AppLayout />
+								</ProtectedRoute>
+							}>
+							<Route index element={<Navigate replace to="/dashboard" />} />
+							<Route path="dashboard" element={<Dashboard />} />
+							<Route path="bookings" element={<Bookings />} />
+							<Route path="bookings/:bookingId" element={<Booking />} />
+							<Route path="checkin/:bookingId" element={<CheckIn />} />
+							<Route path="cabins" element={<Cabins />} />
+							<Route path="users" element={<Users />} />
+							<Route path="settings" element={<Settings />} />
+							<Route path="account" element={<Account />} />
+						</Route>
+						<Route path="login" element={<Login />} />
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</Suspense>
 			</BrowserRouter>
 			<Toaster
 				position="top-center"
