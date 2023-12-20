5ec2b2752bc617ec472bac28bd156cce9a81bdfc
Milad Sadeghi <miladsadeghidm@gmail.com>
2023-12-20 14:02:11 +0330

    Adding first e2e test

---
+++ tests/HomePage/homePage.spec.cjs
@@ -0,0 +1,8 @@
+// @ts-check
+const {test, expect} = require("@playwright/test");
+
+test("Redirect from / to /dashboard", async ({page}) => {
+	await page.goto("/");
+	await page.getByText("Login").click();
+	await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+});
