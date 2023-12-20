4144f6eb33a28e796218a44ae67823df46ef6feb
Milad Sadeghi <miladsadeghidm@gmail.com>
2023-12-20 14:39:21 +0330

    Minor change

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -72,7 +72,7 @@ module.exports = defineConfig({
 	/* Run your local dev server before starting the tests */
 	webServer: {
 	  command: 'npm run dev',
-	  url: 'http://127.0.0.1:5173',
-	  reuseExistingServer: !process.env.CI,
+	  url: 'http://localhost:5173',
+	  reuseExistingServer: true,
 	},
 });
