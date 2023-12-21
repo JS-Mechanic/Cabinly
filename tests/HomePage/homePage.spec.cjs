63e74f22c1615f46a94c463036966c8a9ca43a3e
Milad Sadeghi <miladsadeghidm@gmail.com>
2023-12-21 20:26:13 +0330

    Adding check for url + formatting with prettier

--- tests/HomePage/homePage.spec.cjs
+++ tests/HomePage/homePage.spec.cjs
@@ -4,5 +4,6 @@ const {test, expect} = require("@playwright/test");
 test("Redirect from / to /dashboard", async ({page}) => {
 	await page.goto("/");
 	await page.getByText("Login").click();
-	await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+	await expect(page).toHaveURL(/.*dashboard/);
+	await expect(page.getByRole("heading", {name: "Dashboard"})).toBeVisible();
 });
