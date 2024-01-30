567e282a46f663d21941dd40765472232d4ab38c
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-01-30 12:19:50 +0330

    Adding a test which checks settings blur feature

--- tests/Settings/settings.spec.cjs
+++ tests/Settings/settings.spec.cjs
@@ -4,8 +4,15 @@ const {test, expect} = require("@playwright/test");
 test("checks that settings are successfully fetched", async ({page}) => {
 	await page.goto("/");
 	await page.getByText("Login").click();
-	// await expect(page).toHaveURL(/.*dashboard/);
 	await page.getByText("Settings").click();
 	await expect(page.getByRole("heading", {name: "Update hotel settings"})).toBeVisible();
 	await expect(page.locator("input")).toHaveCount(4);
+});
+
+test("Checks blur feature to update settings", async ({page}) => {
+	await page.goto("/");
+	await page.getByText("Login").click();
+	await page.getByText("Settings").click();
+	await page.locator("#min-nights").fill("5");
+	await expect(page.locator("#min-nights")).toHaveValue("5");
 });
