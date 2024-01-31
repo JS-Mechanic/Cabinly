a72f994ee119a0eeebbdda795fee201ad4309cf2
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-01-31 09:32:09 +0330

    Using custom hook to implementing signUp feature

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,15 +3,17 @@ import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import {useForm} from "react-hook-form";
+import {useSignup} from "./useSignup.js";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-	const {register, formState, getValues, handleSubmit} = useForm();
+	const {signup, isLoading} = useSignup();
+	const {register, formState, getValues, handleSubmit, reset} = useForm();
 	const {errors} = formState;
 
-	function onSubmit(data) {
-		console.log(data);
+	function onSubmit({fullName, email, password}) {
+		signup({fullName, email, password}, {onSettled: () => reset()});
 	}
 
 	return (
