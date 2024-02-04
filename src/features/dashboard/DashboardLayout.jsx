4574c3b56db2fb1045e72a5c12667972e17d70b7
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-02-04 12:30:22 +0330

    Implementing DashboardLayout component

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,37 +1,8 @@
 import styled from "styled-components";
-import {useRecentBookings} from "./useRecentBookings.js";
-import Spinner from "../../ui/Spinner.jsx";
-import {useRecentStays} from "./useRecentStays.js";
-import Stats from "./Stats.jsx";
-import {useCabins} from "../cabins/useCabins.js";
-import SalesChart from "./SalesChart.jsx";
-import DurationChart from "./DurationChart.jsx";
-import TodayActivity from "../check-in-out/TodayActivity.jsx";
 
 const StyledDashboardLayout = styled.div`
-	display: grid;
-	grid-template-columns: 1fr 1fr 1fr 1fr;
-	grid-template-rows: auto 34rem auto;
-	gap: 2.4rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: auto 34rem auto;
+  gap: 2.4rem;
 `;
-
-export default function DashboardLayout() {
-	const {bookings, isLoading: isLoadingBookings} = useRecentBookings();
-	const {confirmedStays, isLoading: isLoadingStays, numDays} = useRecentStays();
-	const {cabins, isLoading: isLoadingCabins} = useCabins();
-	if (isLoadingBookings || isLoadingStays || isLoadingCabins) return <Spinner />;
-
-	return (
-		<StyledDashboardLayout>
-			<Stats
-				bookings={bookings}
-				confirmedStays={confirmedStays}
-				numDays={numDays}
-				cabinCount={cabins.length}
-			/>
-			<TodayActivity />
-			<DurationChart confirmedStays={confirmedStays} />
-			<SalesChart bookings={bookings} numDays={numDays} />
-		</StyledDashboardLayout>
-	);
-}
