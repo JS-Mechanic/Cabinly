8ebfb0c4f09bb6c97cb789e963f12012ce220fb2
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-02-09 19:56:31 +0330

    Disabling react suspense and lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import {lazy, Suspense} from "react";
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
-
-const Dashboard = lazy(() => import("./pages/Dashboard.jsx"));
-const Bookings = lazy(() => import("./pages/Bookings.jsx"));
-const Cabins = lazy(() => import("./pages/Cabins.jsx"));
-const Users = lazy(() => import("./pages/Users.jsx"));
-const Settings = lazy(() => import("./pages/Settings.jsx"));
-const Account = lazy(() => import("./pages/Account.jsx"));
-const Login = lazy(() => import("./pages/Login.jsx"));
-const PageNotFound = lazy(() => import("./pages/PageNotFound.jsx"));
-const AppLayout = lazy(() => import("./ui/AppLayout.jsx"));
-const Booking = lazy(() => import("./pages/Booking.jsx"));
-const CheckIn = lazy(() => import("./pages/CheckIn.jsx"));
-const ProtectedRoute = lazy(() => import("./ui/ProtectedRoute.jsx"));
 
 import GlobalStyles from "./styles/GlobalStyles.js";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import {Toaster} from "react-hot-toast";
 import {DarkModeProvider} from "./context/DarkModeContext.jsx";
+import ProtectedRoute from "./ui/ProtectedRoute.jsx";
+import AppLayout from "./ui/AppLayout.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Bookings from "./pages/Bookings.jsx";
+import Booking from "./pages/Booking.jsx";
+import CheckIn from "./pages/CheckIn.jsx";
+import Cabins from "./pages/Cabins.jsx";
+import Users from "./pages/Users.jsx";
+import Settings from "./pages/Settings.jsx";
+import Login from "./pages/Login.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import Account from "./pages/Account.jsx";
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -35,28 +33,26 @@ export default function App() {
 				<ReactQueryDevtools initialIsOpen={false} />
 				<GlobalStyles />
 				<BrowserRouter>
-					<Suspense fallback={null}>
-						<Routes>
-							<Route
-								element={
-									<ProtectedRoute>
-										<AppLayout />
-									</ProtectedRoute>
-								}>
-								<Route index element={<Navigate replace to="/dashboard" />} />
-								<Route path="dashboard" element={<Dashboard />} />
-								<Route path="bookings" element={<Bookings />} />
-								<Route path="bookings/:bookingId" element={<Booking />} />
-								<Route path="checkin/:bookingId" element={<CheckIn />} />
-								<Route path="cabins" element={<Cabins />} />
-								<Route path="users" element={<Users />} />
-								<Route path="settings" element={<Settings />} />
-								<Route path="account" element={<Account />} />
-							</Route>
-							<Route path="login" element={<Login />} />
-							<Route path="*" element={<PageNotFound />} />
-						</Routes>
-					</Suspense>
+					<Routes>
+						<Route
+							element={
+								<ProtectedRoute>
+									<AppLayout />
+								</ProtectedRoute>
+							}>
+							<Route index element={<Navigate replace to="/dashboard" />} />
+							<Route path="dashboard" element={<Dashboard />} />
+							<Route path="bookings" element={<Bookings />} />
+							<Route path="bookings/:bookingId" element={<Booking />} />
+							<Route path="checkin/:bookingId" element={<CheckIn />} />
+							<Route path="cabins" element={<Cabins />} />
+							<Route path="users" element={<Users />} />
+							<Route path="settings" element={<Settings />} />
+							<Route path="account" element={<Account />} />
+						</Route>
+						<Route path="login" element={<Login />} />
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
 				</BrowserRouter>
 				<Toaster
 					position="top-center"
